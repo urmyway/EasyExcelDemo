f82a71b240092e78442da541c31081c57d9e6e0c
urmyway <zhf960629@vip.qq.com>
2020-06-02 19:18:14 +0800

    不创建对象的读

--- src/main/java/ReadTest.java
+++ src/main/java/ReadTest.java
@@ -164,4 +164,14 @@ public class ReadTest{
         // 这里 需要指定读用哪个class去读，然后读取第一个sheet
         EasyExcel.read(fileName, ExceptionDemoData.class, new DemoExceptionListener()).sheet().doRead();
     }
+
+    /**
+     * 不创建对象的读
+     */
+    @Test
+    public void noModelRead() {
+        String fileName = "D:\\test\\demo.xlsx";
+        // 这里 只要，然后读取第一个sheet 同步读取会自动finish
+        EasyExcel.read(fileName, new NoModelDataListener()).sheet().doRead();
+    }
 }
