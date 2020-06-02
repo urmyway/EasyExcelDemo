28079f35b19354d053109fa9ef3969e0d16b48ea
urmyway <zhf960629@vip.qq.com>
2020-06-02 15:28:10 +0800

    额外信息（批注、超链接、合并单元格信息读取）

--- src/main/java/ReadTest.java
+++ src/main/java/ReadTest.java
@@ -1,5 +1,6 @@
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.enums.CellExtraTypeEnum;
 import com.alibaba.excel.read.metadata.ReadSheet;
 import entity.ConverterData;
 import entity.DemoData;
@@ -100,9 +101,34 @@ public class ReadTest{
     @Test
     public void headerRead() {
         String fileName = "D:\\test\\bb.xlsx";
-        System.out.println(fileName);
-
         // 这里 需要指定读用哪个class去读，然后读取第一个sheet
         EasyExcel.read(fileName, DemoData.class, new DemoHeadDataListener()).sheet().doRead();
+    }
+
+    /**
+     * 额外信息（批注、超链接、合并单元格信息读取）
+     * <p>
+     * 由于是流式读取，没法在读取到单元格数据的时候直接读取到额外信息，所以只能最后通知哪些单元格有哪些额外信息
+     *
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>
+     * 2. 由于默认异步读取excel，所以需要创建excel一行一行的回调监听器，参照{@link DemoExtraListener}
+     * <p>
+     * 3. 直接读即可
+     *
+     * @since 2.2.0-beat1
+     */
+    @Test
+    public void extraRead() {
+        String fileName = "D:\\test\\cc.xlsx";
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet
+        EasyExcel.read(fileName, DemoData.class, new DemoExtraListener())
+                // 需要读取批注 默认不读取
+                .extraRead(CellExtraTypeEnum.COMMENT)
+                // 需要读取超链接 默认不读取
+                .extraRead(CellExtraTypeEnum.HYPERLINK)
+                // 需要读取合并单元格信息 默认不读取
+                .extraRead(CellExtraTypeEnum.MERGE).sheet().doRead();
     }
 }
---
+++ src/main/java/listener/DemoExtraListener.java
@@ -0,0 +1,108 @@
+package listener;
+
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.excel.metadata.CellExtra;
+import com.alibaba.fastjson.JSON;
+import dao.DemoDAO;
+import entity.DemoData;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 有个很重要的点 DemoExtraListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
+@Slf4j
+public class DemoExtraListener extends AnalysisEventListener<DemoData> {
+    /**
+     * 每隔5条存储数据库，实际使用中可以3000条，然后清理list ，方便内存回收
+     */
+    private static final int BATCH_COUNT = 5;
+    List<DemoData> list = new ArrayList<DemoData>();
+    /**
+     * 假设这个是一个DAO，当然有业务逻辑这个也可以是一个service。当然如果不用存储这个对象没用。
+     */
+    private DemoDAO demoDAO;
+    public DemoExtraListener() {
+        // 这里是demo，所以随便new一个。实际使用如果到了spring,请使用下面的有参构造函数
+        demoDAO = new DemoDAO();
+    }
+    /**
+     * 如果使用了spring,请使用这个构造方法。每次创建Listener的时候需要把spring管理的类传进来
+     *
+     * @param demoDAO
+     */
+    public DemoExtraListener(DemoDAO demoDAO) {
+        this.demoDAO = demoDAO;
+    }
+    /**
+     * 这个每一条数据解析都会来调用
+     *
+     * @param data
+     *            one row value. Is is same as {@link AnalysisContext#readRowHolder()}
+     * @param context
+     */
+    @Override
+    public void invoke(DemoData data, AnalysisContext context) {
+        log.info("解析到一条数据:{}", JSON.toJSONString(data));
+        list.add(data);
+        // 达到BATCH_COUNT了，需要去存储一次数据库，防止数据几万条数据在内存，容易OOM
+        if (list.size() >= BATCH_COUNT) {
+            saveData();
+            // 存储完成清理 list
+            list.clear();
+        }
+    }
+    /**
+     * 所有数据解析完成了 都会来调用
+     *
+     * @param context
+     */
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+        // 这里也要保存数据，确保最后遗留的数据也存储到数据库
+        saveData();
+        log.info("所有数据解析完成！");
+    }
+    /**
+     * 加上存储数据库
+     */
+    private void saveData() {
+        log.info("{}条数据，开始存储数据库！", list.size());
+        demoDAO.save(list);
+        log.info("存储数据库成功！");
+    }
+
+    @Override
+    public void extra(CellExtra extra, AnalysisContext context) {
+        log.info("读取到了一条额外信息:{}", JSON.toJSONString(extra));
+        switch (extra.getType()) {
+            case COMMENT:
+                log.info("额外信息是批注,在rowIndex:{},columnIndex;{},内容是:{}", extra.getRowIndex(), extra.getColumnIndex(),
+                        extra.getText());
+                break;
+            case HYPERLINK:
+                if ("Sheet1!A1".equals(extra.getText())) {
+                    log.info("额外信息是超链接,在rowIndex:{},columnIndex;{},内容是:{}", extra.getRowIndex(),
+                            extra.getColumnIndex(), extra.getText());
+                } else if ("Sheet2!A1".equals(extra.getText())) {
+                    log.info(
+                            "额外信息是超链接,而且覆盖了一个区间,在firstRowIndex:{},firstColumnIndex;{},lastRowIndex:{},lastColumnIndex:{},"
+                                    + "内容是:{}",
+                            extra.getFirstRowIndex(), extra.getFirstColumnIndex(), extra.getLastRowIndex(),
+                            extra.getLastColumnIndex(), extra.getText());
+                } else {
+                    Assert.fail("Unknown hyperlink!");
+                }
+                break;
+            case MERGE:
+                log.info(
+                        "额外信息是超链接,而且覆盖了一个区间,在firstRowIndex:{},firstColumnIndex;{},lastRowIndex:{},lastColumnIndex:{}",
+                        extra.getFirstRowIndex(), extra.getFirstColumnIndex(), extra.getLastRowIndex(),
+                        extra.getLastColumnIndex());
+                break;
+            default:
+        }
+    }
+}
