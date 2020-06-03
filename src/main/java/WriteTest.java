034ce89e8fe0065cd5946fd5b594fe5b3388778c
urmyway <zhf960629@vip.qq.com>
2020-06-03 15:35:07 +0800

    插入批注

--- src/main/java/WriteTest.java
+++ src/main/java/WriteTest.java
@@ -9,6 +9,7 @@ import com.alibaba.excel.write.metadata.style.WriteFont;
 import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import entity.*;
+import listener.CommentWriteHandler;
 import listener.CustomCellWriteHandler;
 import listener.CustomSheetWriteHandler;
 import org.apache.poi.ss.usermodel.FillPatternType;
@@ -435,5 +436,23 @@ public class WriteTest {
         // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
         EasyExcel.write(fileName, Demo.class).registerWriteHandler(new CustomSheetWriteHandler())
                 .registerWriteHandler(new CustomCellWriteHandler()).sheet("模板").doWrite(data());
+    }
+
+    /**
+     * 插入批注
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link Demo}
+     * <p>
+     * 2. 注册拦截器 {@link CommentWriteHandler}
+     * <p>
+     * 2. 直接写即可
+     */
+    @Test
+    public void commentWrite() {
+        String fileName = "D:\\test\\" + "commentWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 这里要注意inMemory 要设置为true，才能支持批注。目前没有好的办法解决 不在内存处理批注。这个需要自己选择。
+        EasyExcel.write(fileName, Demo.class).inMemory(Boolean.TRUE).registerWriteHandler(new CommentWriteHandler())
+                .sheet("模板").doWrite(data());
     }
 }
