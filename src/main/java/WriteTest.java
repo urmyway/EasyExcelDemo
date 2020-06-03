6679512e7fbc615d594975d64c529678897d5fbd
urmyway <zhf960629@vip.qq.com>
2020-06-03 15:26:19 +0800

    自定义拦截器（上面几点都不符合但是要对单元格进行操作的参照这个）增加表头超链接、行数据下拉框选项

--- src/main/java/WriteTest.java
+++ src/main/java/WriteTest.java
@@ -9,6 +9,8 @@ import com.alibaba.excel.write.metadata.style.WriteFont;
 import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import entity.*;
+import listener.CustomCellWriteHandler;
+import listener.CustomSheetWriteHandler;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.junit.Test;
@@ -414,5 +416,24 @@ public class WriteTest {
             list.add(data);
         }
         return list;
+    }
+
+    /**
+     * 下拉，超链接等自定义拦截器（上面几点都不符合但是要对单元格进行操作的参照这个）
+     * <p>
+     * demo这里实现2点。1. 对第一行第一列的头超链接到:https://github.com/alibaba/easyexcel 2. 对第一列第一行和第二行的数据新增下拉框，显示 测试1 测试2
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link Demo}
+     * <p>
+     * 2. 注册拦截器 {@link CustomCellWriteHandler} {@link CustomSheetWriteHandler}
+     * <p>
+     * 2. 直接写即可
+     */
+    @Test
+    public void customHandlerWrite() {
+        String fileName = "D:\\test\\" + "customHandlerWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, Demo.class).registerWriteHandler(new CustomSheetWriteHandler())
+                .registerWriteHandler(new CustomCellWriteHandler()).sheet("模板").doWrite(data());
     }
 }
