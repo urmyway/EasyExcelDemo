2b2fd7194a710ab2e753c878c5c8cc5db4091242
urmyway <zhf960629@vip.qq.com>
2020-06-03 15:16:33 +0800

    自动列宽(不太精确)

--- src/main/java/WriteTest.java
+++ src/main/java/WriteTest.java
@@ -7,6 +7,7 @@ import com.alibaba.excel.write.metadata.WriteTable;
 import com.alibaba.excel.write.metadata.style.WriteCellStyle;
 import com.alibaba.excel.write.metadata.style.WriteFont;
 import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
+import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import entity.*;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.IndexedColors;
@@ -377,6 +378,41 @@ public class WriteTest {
         list.add(head0);
         list.add(head1);
         list.add(head2);
+        return list;
+    }
+
+    /**
+     * 自动列宽(不太精确)
+     * <p>
+     * 这个目前不是很好用，比如有数字就会导致换行。而且长度也不是刚好和实际长度一致。 所以需要精确到刚好列宽的慎用。 当然也可以自己参照
+     * {@link LongestMatchColumnWidthStyleStrategy}重新实现.
+     * <p>
+     * poi 自带{@link @SXSSFSheet#autoSizeColumn(int)} 对中文支持也不太好。目前没找到很好的算法。 有的话可以推荐下。
+     *
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link LongestMatchColumnWidthData}
+     * <p>
+     * 2. 注册策略{@link LongestMatchColumnWidthStyleStrategy}
+     * <p>
+     * 3. 直接写即可
+     */
+    @Test
+    public void longestMatchColumnWidthWrite() {
+        String fileName = "D:\\test\\" + "longestMatchColumnWidthWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, LongestMatchColumnWidthData.class)
+                .registerWriteHandler(new LongestMatchColumnWidthStyleStrategy()).sheet("模板").doWrite(dataLong());
+    }
+
+    private List<LongestMatchColumnWidthData> dataLong() {
+        List<LongestMatchColumnWidthData> list = new ArrayList<LongestMatchColumnWidthData>();
+        for (int i = 0; i < 10; i++) {
+            LongestMatchColumnWidthData data = new LongestMatchColumnWidthData();
+            data.setString("测试很长的字符串测试很长的字符串测试很长的字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(1000000000000.0);
+            list.add(data);
+        }
         return list;
     }
 }
