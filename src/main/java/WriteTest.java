d29eb8225f7bc0ffdd654bb3d6c5b1a5becc2719
urmyway <zhf960629@vip.qq.com>
2020-06-03 12:00:19 +0800

    自定义样式

--- src/main/java/WriteTest.java
+++ src/main/java/WriteTest.java
@@ -2,7 +2,12 @@ import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.util.FileUtils;
 import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
+import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import entity.*;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.junit.Test;
 import util.TestFileUtil;
 
@@ -234,5 +239,57 @@ public class WriteTest {
         String fileName = "D:\\test\\" + "widthAndHeightWrite" + System.currentTimeMillis() + ".xlsx";
         // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
         EasyExcel.write(fileName, WidthAndHeightData.class).sheet("模板").doWrite(data());
+    }
+
+    /**
+     * 注解形式自定义样式
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link DemoStyleData}
+     * <p>
+     * 3. 直接写即可
+     *
+     * @since 2.2.0-beta1
+     */
+    @Test
+    public void annotationStyleWrite() {
+        String fileName = "D:\\test\\" + "annotationStyleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, DemoStyleData.class).sheet("模板").doWrite(data());
+    }
+
+    /**
+     * 自定义样式
+     * <p>1. 创建excel对应的实体对象 参照{@link Demo}
+     * <p>2. 创建一个style策略 并注册
+     * <p>3. 直接写即可
+     */
+    @Test
+    public void styleWrite() {
+        String fileName = "D:\\test\\" + "styleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 头的策略
+        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
+        // 背景设置为红色
+        headWriteCellStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
+        WriteFont headWriteFont = new WriteFont();
+        //头字体设置成20
+        headWriteFont.setFontHeightInPoints((short)20);
+        headWriteCellStyle.setWriteFont(headWriteFont);
+        // 内容的策略
+        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
+        // 这里需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.头默认了 FillPatternType所以可以不指定
+        contentWriteCellStyle.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+        // 背景绿色
+        contentWriteCellStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+        WriteFont contentWriteFont = new WriteFont();
+        // 字体大小
+        contentWriteFont.setFontHeightInPoints((short)20);
+        contentWriteCellStyle.setWriteFont(contentWriteFont);
+        // 这个策略是 头是头的样式 内容是内容的样式 其他的策略可以自己实现
+        HorizontalCellStyleStrategy horizontalCellStyleStrategy =
+                new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
+
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, Demo.class).registerWriteHandler(horizontalCellStyleStrategy).sheet("模板")
+                .doWrite(data());
     }
 }
