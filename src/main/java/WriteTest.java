43401d93a61079ee3d45a6cf99d67c9b02a29d3c
urmyway <zhf960629@vip.qq.com>
2020-06-03 10:53:59 +0800

    重复多次写入

--- src/main/java/WriteTest.java
+++ src/main/java/WriteTest.java
@@ -103,4 +103,61 @@ public class WriteTest {
         // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
         EasyExcel.write(fileName, ComplexHeadData.class).sheet("模板").doWrite(data());
     }
+
+    /**
+     * 重复多次写入
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link ComplexHeadData}
+     * <p>
+     * 2. 使用{@link @ExcelProperty}注解指定复杂的头
+     * <p>
+     * 3. 直接调用二次写入即可
+     */
+    @Test
+    public void repeatedWrite() {
+        // 方法1 如果写到同一个sheet
+        String fileName = "D:\\test\\" + "repeatedWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = EasyExcel.write(fileName, Demo.class).build();
+        // 这里注意 如果同一个sheet只要创建一次
+        WriteSheet writeSheet = EasyExcel.writerSheet("模板").build();
+        // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来
+        for (int i = 0; i < 5; i++) {
+            // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
+            List<Demo> data = data();
+            excelWriter.write(data, writeSheet);
+        }
+        /// 千万别忘记finish 会帮忙关闭流
+        excelWriter.finish();
+
+        // 方法2 如果写到不同的sheet 同一个对象
+        fileName = "D:\\test\\" + "repeatedWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 指定文件
+        excelWriter = EasyExcel.write(fileName, Demo.class).build();
+        // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来。这里最终会写到5个sheet里面
+        for (int i = 0; i < 5; i++) {
+            // 每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样
+            writeSheet = EasyExcel.writerSheet(i, "模板" + i).build();
+            // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
+            List<Demo> data = data();
+            excelWriter.write(data, writeSheet);
+        }
+        /// 千万别忘记finish 会帮忙关闭流
+        excelWriter.finish();
+
+        // 方法3 如果写到不同的sheet 不同的对象
+        fileName = "D:\\test\\" + "repeatedWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 指定文件
+        excelWriter = EasyExcel.write(fileName).build();
+        // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来。这里最终会写到5个sheet里面
+        for (int i = 0; i < 5; i++) {
+            // 每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样。这里注意Demo.class 可以每次都变，我这里为了方便 所以用的同一个class 实际上可以一直变
+            writeSheet = EasyExcel.writerSheet(i, "模板" + i).head(Demo.class).build();
+            // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
+            List<Demo> data = data();
+            excelWriter.write(data, writeSheet);
+        }
+        /// 千万别忘记finish 会帮忙关闭流
+        excelWriter.finish();
+    }
 }
