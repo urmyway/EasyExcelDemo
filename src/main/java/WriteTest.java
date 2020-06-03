533bc3f9f766964f7915ed00f69d8df512b9cec3
urmyway <zhf960629@vip.qq.com>
2020-06-03 15:06:00 +0800

    使用table去写入

--- src/main/java/WriteTest.java
+++ src/main/java/WriteTest.java
@@ -3,6 +3,7 @@ import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.util.FileUtils;
 import com.alibaba.excel.write.merge.LoopMergeStrategy;
 import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.WriteTable;
 import com.alibaba.excel.write.metadata.style.WriteCellStyle;
 import com.alibaba.excel.write.metadata.style.WriteFont;
 import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
@@ -319,5 +320,29 @@ public class WriteTest {
         LoopMergeStrategy loopMergeStrategy = new LoopMergeStrategy(2, 0);
         // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
         EasyExcel.write(fileName, Demo.class).registerWriteHandler(loopMergeStrategy).sheet("模板").doWrite(data());
+    }
+
+    /**
+     * 使用table去写入
+     * <p>1. 创建excel对应的实体对象 参照{@link DemoData}
+     * <p>2. 然后写入table即可
+     */
+    @Test
+    public void tableWrite() {
+        String fileName = "D:\\test\\" + "tableWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里直接写多个table的案例了，如果只有一个 也可以直一行代码搞定，参照其他案例
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = EasyExcel.write(fileName, Demo.class).build();
+        // 把sheet设置为不需要头 不然会输出sheet的头 这样看起来第一个table 就有2个头了
+        WriteSheet writeSheet = EasyExcel.writerSheet("模板").needHead(Boolean.FALSE).build();
+        // 这里必须指定需要头，table 会继承sheet的配置，sheet配置了不需要，table 默认也是不需要
+        WriteTable writeTable0 = EasyExcel.writerTable(0).needHead(Boolean.TRUE).build();
+        WriteTable writeTable1 = EasyExcel.writerTable(1).needHead(Boolean.TRUE).build();
+        // 第一次写入会创建头
+        excelWriter.write(data(), writeSheet, writeTable0);
+        // 第二次写如也会创建头，然后在第一次的后面写入数据
+        excelWriter.write(data(), writeSheet, writeTable1);
+        // 千万别忘记finish 会帮忙关闭流
+        excelWriter.finish();
     }
 }
