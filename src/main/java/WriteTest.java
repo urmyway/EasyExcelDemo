32ddae8899247c8da33be4998f1a1137fe84341f
urmyway <zhf960629@vip.qq.com>
2020-06-03 11:14:41 +0800

    日期、数字或者自定义格式转换

--- src/main/java/WriteTest.java
+++ src/main/java/WriteTest.java
@@ -1,10 +1,7 @@
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.write.metadata.WriteSheet;
-import entity.ComplexHeadData;
-import entity.Demo;
-import entity.DemoData;
-import entity.IndexData;
+import entity.*;
 import org.junit.Test;
 import util.TestFileUtil;
 
@@ -159,5 +156,18 @@ public class WriteTest {
         }
         /// 千万别忘记finish 会帮忙关闭流
         excelWriter.finish();
+    }
+
+    /**
+     * 日期、数字或者自定义格式转换
+     * <p>1. 创建excel对应的实体对象 参照{@link ConverterData}
+     * <p>2. 使用{@link @ExcelProperty}配合使用注解{@link @DateTimeFormat}、{@link @NumberFormat}或者自定义注解
+     * <p>3. 直接写即可
+     */
+    @Test
+    public void converterWrite() {
+        String fileName = "D:\\test\\" + "converterWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, ConverterData.class).sheet("模板").doWrite(data());
     }
 }
