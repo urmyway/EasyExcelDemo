633781c99007ac92ab841892973c6266baa0be51
urmyway <zhf960629@vip.qq.com>
2020-06-03 15:01:57 +0800

    合并单元格

--- src/main/java/WriteTest.java
+++ src/main/java/WriteTest.java
@@ -1,6 +1,7 @@
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.util.FileUtils;
+import com.alibaba.excel.write.merge.LoopMergeStrategy;
 import com.alibaba.excel.write.metadata.WriteSheet;
 import com.alibaba.excel.write.metadata.style.WriteCellStyle;
 import com.alibaba.excel.write.metadata.style.WriteFont;
@@ -291,5 +292,32 @@ public class WriteTest {
         // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
         EasyExcel.write(fileName, Demo.class).registerWriteHandler(horizontalCellStyleStrategy).sheet("模板")
                 .doWrite(data());
+    }
+
+    /**
+     * 合并单元格
+     * <p>
+     * 1. 创建excel对应的实体对象 参照{@link DemoData} {@link DemoMergeData}
+     * <p>
+     * 2. 创建一个merge策略 并注册
+     * <p>
+     * 3. 直接写即可
+     *
+     * @since 2.2.0-beta1
+     */
+    @Test
+    public void mergeWrite() {
+        // 方法1 注解
+        String fileName = "D:\\test\\" + "mergeWrite" + System.currentTimeMillis() + ".xlsx";
+        // 在DemoStyleData里面加上ContentLoopMerge注解
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, DemoMergeData.class).sheet("模板").doWrite(data());
+
+        // 方法2 自定义合并单元格策略
+        fileName = "D:\\test\\" + "mergeWrite" + System.currentTimeMillis() + ".xlsx";
+        // 每隔2行会合并 把eachColumn 设置成 3 也就是我们数据的长度，所以就第一列会合并。当然其他合并策略也可以自己写
+        LoopMergeStrategy loopMergeStrategy = new LoopMergeStrategy(2, 0);
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        EasyExcel.write(fileName, Demo.class).registerWriteHandler(loopMergeStrategy).sheet("模板").doWrite(data());
     }
 }
