43eb3ba50b789efd22c4ee848085932a0b8da6bb
urmyway <zhf960629@vip.qq.com>
2020-06-04 14:34:13 +0800

    复杂的填充

--- src/main/java/FillTest.java
+++ src/main/java/FillTest.java
@@ -74,4 +74,32 @@ public class FillTest {
         return list;
     }
 
+    /**
+     * 复杂的填充
+     *
+     * @since 2.1.1
+     */
+    @Test
+    public void complexFill() {
+        // 模板注意 用{} 来表示你要用的变量 如果本来就有"{","}" 特殊字符 用"\{","\}"代替
+        // {} 代表普通变量 {.} 代表是list的变量
+        String templateFileName = "D:\\test\\complex.xlsx";
+
+        String fileName = "D:\\test\\" + "complexFill" + System.currentTimeMillis() + ".xlsx";
+        ExcelWriter excelWriter = EasyExcel.write(fileName).withTemplate(templateFileName).build();
+        WriteSheet writeSheet = EasyExcel.writerSheet().build();
+        // 这里注意 入参用了forceNewRow 代表在写入list的时候不管list下面有没有空行 都会创建一行，然后下面的数据往后移动。默认 是false，会直接使用下一行，如果没有则创建。
+        // forceNewRow 如果设置了true,有个缺点 就是他会把所有的数据都放到内存了，所以慎用
+        // 简单的说 如果你的模板有list,且list不是最后一行，下面还有数据需要填充 就必须设置 forceNewRow=true 但是这个就会把所有数据放到内存 会很耗内存
+        // 如果数据量大 list不是最后一行 参照下一个 √
+        FillConfig fillConfig = FillConfig.builder().forceNewRow(Boolean.TRUE).build();
+        excelWriter.fill(data(), fillConfig, writeSheet);
+        excelWriter.fill(data(), fillConfig, writeSheet);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("date", "2019年10月9日13:28:28");
+        map.put("total", 1000);
+        //分多次填充
+        excelWriter.fill(map, writeSheet);
+        excelWriter.finish();
+    }
 }
