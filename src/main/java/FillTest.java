3aad16e1ce1cdeecee615359bf2fd3fa7b2fd623
urmyway <zhf960629@vip.qq.com>
2020-06-04 14:27:35 +0800

    填充列表

--- src/main/java/FillTest.java
+++ src/main/java/FillTest.java
@@ -1,11 +1,14 @@
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.fill.FillConfig;
+import entity.Demo;
 import entity.FillData;
 import org.junit.Test;
 import util.TestFileUtil;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class FillTest {
     /**
@@ -31,6 +34,44 @@ public class FillTest {
         map.put("name", "张三");
         map.put("number", 5.2);
         EasyExcel.write(fileName).withTemplate(templateFileName).sheet().doFill(map);
+    }
+
+    /**
+     * 填充列表
+     *
+     * @since 2.1.1
+     */
+    @Test
+    public void listFill() {
+        // 模板注意 用{} 来表示你要用的变量 如果本来就有"{","}" 特殊字符 用"\{","\}"代替
+        // 填充list 的时候还要注意 模板中{.} 多了个点 表示list
+        String templateFileName = "D:\\test\\list.xlsx";
+
+        // 方案1 一下子全部放到内存里面 并填充
+        String fileName = "D:\\test\\" + "listFill" + System.currentTimeMillis() + ".xlsx";
+        // 这里 会填充到第一个sheet， 然后文件流会自动关闭
+        EasyExcel.write(fileName).withTemplate(templateFileName).sheet().doFill(data());
+
+        // 方案2 分多次 填充 会使用文件缓存（省内存）
+        fileName = "D:\\test\\" + "listFill" + System.currentTimeMillis() + ".xlsx";
+        ExcelWriter excelWriter = EasyExcel.write(fileName).withTemplate(templateFileName).build();
+        WriteSheet writeSheet = EasyExcel.writerSheet().build();
+        excelWriter.fill(data(), writeSheet);
+        excelWriter.fill(data(), writeSheet);
+        // 千万别忘记关闭流
+        excelWriter.finish();
+    }
+
+    //通用数据生成
+    private List<FillData> data() {
+        List<FillData> list = new ArrayList<FillData>();
+        for (int i = 0; i < 10; i++) {
+            FillData fillData = new FillData();
+            list.add(fillData);
+            fillData.setName("张三");
+            fillData.setNumber(5.2);
+        }
+        return list;
     }
 
 }
