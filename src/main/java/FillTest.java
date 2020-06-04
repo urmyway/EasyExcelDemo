5cc9d6477b5497c564a56307d185786b8f841e62
urmyway <zhf960629@vip.qq.com>
2020-06-04 15:15:13 +0800

    数据量大的复杂填充

--- src/main/java/FillTest.java
+++ src/main/java/FillTest.java
@@ -4,6 +4,7 @@ import com.alibaba.excel.write.metadata.WriteSheet;
 import com.alibaba.excel.write.metadata.fill.FillConfig;
 import entity.Demo;
 import entity.FillData;
+import entity.complexFillWithTable;
 import org.junit.Test;
 import util.TestFileUtil;
 
@@ -88,9 +89,9 @@ public class FillTest {
         String fileName = "D:\\test\\" + "complexFill" + System.currentTimeMillis() + ".xlsx";
         ExcelWriter excelWriter = EasyExcel.write(fileName).withTemplate(templateFileName).build();
         WriteSheet writeSheet = EasyExcel.writerSheet().build();
-        // 这里注意 入参用了forceNewRow 代表在写入list的时候不管list下面有没有空行 都会创建一行，然后下面的数据往后移动。默认 是false，会直接使用下一行，如果没有则创建。
+        // 这里注意 入参用了forceNewRow 代表在写入list的时候不管list下面有没有空行 都会创建一行，然后下面的数据往后移动(多了个移动，数据保留在内存中)。默认 是false，会直接使用下一行，如果没有则创建。
         // forceNewRow 如果设置了true,有个缺点 就是他会把所有的数据都放到内存了，所以慎用
-        // 简单的说 如果你的模板有list,且list不是最后一行，下面还有数据需要填充 就必须设置 forceNewRow=true 但是这个就会把所有数据放到内存 会很耗内存
+        // 简单的说 如果你的模板有list,且list不是最后一行，下面还有数据需要填充 就必须设置 forceNewRow=true 但是这个就会把所有数据放到内存 会很耗内存 √
         // 如果数据量大 list不是最后一行 参照下一个 √
         FillConfig fillConfig = FillConfig.builder().forceNewRow(Boolean.TRUE).build();
         excelWriter.fill(data(), fillConfig, writeSheet);
@@ -101,5 +102,56 @@ public class FillTest {
         //分多次填充
         excelWriter.fill(map, writeSheet);
         excelWriter.finish();
+    }
+
+    /**
+     * 数据量大的复杂填充
+     * <p>
+     * 这里的解决方案是 确保模板list为最后一行，然后再拼接table.还有03版没救，只能刚正面加内存 haha。
+     *
+     * @since 2.1.1
+     */
+    @Test
+    public void complexFillWithTable() {
+        // 模板注意 用{} 来表示你要用的变量 如果本来就有"{","}" 特殊字符 用"\{","\}"代替
+        // {} 代表普通变量 {.} 代表是list的变量
+        // 这里模板 删除了list以后的数据，也就是统计的这一行 √
+        String templateFileName = "D:\\test\\complexFillWithTable.xlsx";
+        String fileName = "D:\\test\\" + "complexFillWithTable" + System.currentTimeMillis() + ".xlsx";
+        ExcelWriter excelWriter = EasyExcel.write(fileName).withTemplate(templateFileName).build();
+        WriteSheet writeSheet = EasyExcel.writerSheet().build();
+        // 直接写入数据
+        excelWriter.fill(data(), writeSheet);
+        excelWriter.fill(data(), writeSheet);
+
+        // 写入list之前的数据
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("date", "2020年10月9日13:28:28");
+        excelWriter.fill(map, writeSheet);
+
+        // list 后面还有个统计 想办法手动写入 √
+        // 这里偷懒直接用list 也可以用对象
+/*        List<List<String>> totalListList = new ArrayList<List<String>>();
+        List<String> totalList = new ArrayList<String>();
+        totalListList.add(totalList);
+        totalList.add(null);
+        totalList.add(null);
+        totalList.add(null);
+        // 第四列
+        totalList.add("统计:1000");
+        // 这里是write 别和fill 搞错了
+        excelWriter.write(totalListList, writeSheet);*/
+        //对象
+        ArrayList<Object> list = new ArrayList<>();
+        complexFillWithTable object = new complexFillWithTable();
+        object.setName(null);
+        object.setNumber(null);
+        object.setName1(null);
+        object.setNumber1("统计:1000");
+        list.add(object);
+        excelWriter.write(list,writeSheet);
+        excelWriter.finish();
+        // 总体上写法比较复杂 但是也没有想到好的版本 异步的去写入excel 不支持行的删除和移动，也不支持备注这种的写入，所以也排除了可以
+        // 新建一个 然后一点点复制过来的方案，最后导致list需要新增行的时候，后面的列的数据没法后移，后续会继续想想解决方案
     }
 }
---
+++ src/main/java/entity/complexFillWithTable.java
@@ -0,0 +1,11 @@
+package entity;
+
+import lombok.Data;
+
+@Data
+public class complexFillWithTable {
+    private String name;
+    private String number;
+    private String name1;
+    private String number1;
+}
